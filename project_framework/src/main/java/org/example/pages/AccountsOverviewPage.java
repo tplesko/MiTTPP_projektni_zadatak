5d0975575fa3ee17306d9c3bd33ecab94b4f668f
tplesko <tea.plesko15@gmail.com>
2025-02-06 17:27:59 +0100

    Added Accounts Overviev class

---
+++ project_framework/src/main/java/org/example/pages/AccountsOverviewPage.java
@@ -0,0 +1,29 @@
+package org.example.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.NoSuchElementException;
+
+public class AccountsOverviewPage {
+    private WebDriver driver;
+
+    private By accountsOverviewHeader = By.xpath("//h1[contains(text(),'Accounts Overview')]");
+    private By logoutLink = By.xpath("//*[@id='leftPanel']/ul/li[8]/a");
+
+    public AccountsOverviewPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getAccountsOverviewHeaderText() {
+        return driver.findElement(accountsOverviewHeader).getText();
+    }
+
+    public boolean isUserLoggedIn() {
+        try {
+            return driver.findElement(logoutLink).isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+}
